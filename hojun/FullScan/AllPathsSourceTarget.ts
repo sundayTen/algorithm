a7a7af804ac331a0166414b9afe3f4dea7979b69
hojunin <dlsghwns@naver.com>
2022-06-12 10:24:00 +0900

    필요없는 조건 제거

--- hojun/FullScan/AllPathsSourceTarget.ts
+++ hojun/FullScan/AllPathsSourceTarget.ts
@@ -5,20 +5,20 @@ type Graph = Path[];
 const isDone = (endNode: NodeVal, node: NodeVal) => endNode === node;
 
 const allPathsSourceTarget = (graph: Graph) => {
-  const end = graph.length - 1;
-  const foundPath: Path[] = [];
+    const end = graph.length - 1;
+    const foundPath: Path[] = [];
 
-  const findPath = (path: Path, targetIndex: NodeVal) => {
-    const newPath = [...path, targetIndex];
+    const findPath = (path: Path, targetIndex: NodeVal) => {
+        const newPath = [...path, targetIndex];
 
-    if (isDone(end, targetIndex)) {
-      foundPath.push(newPath);
-    }
+        if (isDone(end, targetIndex)) {
+            foundPath.push(newPath);
+        }
 
-    graph[targetIndex].forEach((node) => findPath(newPath, node));
-  };
+        graph[targetIndex].forEach((node) => findPath(newPath, node));
+    };
 
-  graph[0].forEach((node) => findPath([0], node));
+    graph[0].forEach((node) => findPath([0], node));
 
-  return foundPath.filter((path) => path[0] === 0);
+    return foundPath;
 };
