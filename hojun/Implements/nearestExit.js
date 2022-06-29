e78c8f4ff9402cb84754a3b76309712027a42274
hojunin <dlsghwns@naver.com>
2022-06-30 01:08:14 +0900

    E_A_S_Y

--- hojun/Implements/nearestExit.js
+++ hojun/Implements/nearestExit.js
@@ -1,48 +1,57 @@
 const isExitGenerator = (sizeX, sizeY) => (i, j) => {
-  if (i < 0 || i >= sizeY) {
-    return true;
-  }
+    if (i < 0 || i >= sizeY) {
+        return true;
+    }
 
-  if (j < 0 || j >= sizeX) {
-    return true;
-  }
+    if (j < 0 || j >= sizeX) {
+        return true;
+    }
 
-  return false;
+    return false;
 };
 
 const nearestExit = (maze, entrance) => {
-  let answer = [];
-  let route = [];
-  const isExit = isExitGenerator(maze[0].length, maze.length);
+    let answer = [];
+    const isExit = isExitGenerator(maze[0].length, maze.length);
 
-  const visit = (visitedMaze, room, step) => {
-    const [i, j] = room;
-    const isEntrance = room[0] === entrance[0] && room[1] === entrance[1];
+    const visit = (visitedMaze, room, step) => {
+        const [i, j] = room;
+        const isEntrance = room[0] === entrance[0] && room[1] === entrance[1];
 
-    visitedMaze[i][j] = "+";
+        visitedMaze[i][j] = '+';
 
-    [
-      [i, j - 1],
-      [i, j + 1],
-      [i - 1, j],
-      [i + 1, j],
-    ].forEach((nextRoom) => {
-      const [ni, nj] = nextRoom;
+        [
+            [i, j - 1],
+            [i, j + 1],
+            [i - 1, j],
+            [i + 1, j],
+        ].forEach((nextRoom) => {
+            const [ni, nj] = nextRoom;
 
-      if (isExit(ni, nj)) {
-        if (!isEntrance) {
-          answer.push(step);
-          route.push(nextRoom);
-        }
-        return;
-      }
+            if (isExit(ni, nj)) {
+                if (!isEntrance) {
+                    answer.push(step);
+                }
+                return;
+            }
 
-      if (visitedMaze[ni][nj] === ".") {
-        visit(visitedMaze, nextRoom, step + 1);
-      }
-    });
-  };
+            if (visitedMaze[ni][nj] === '.') {
+                visit(visitedMaze, nextRoom, step + 1);
+            }
+        });
+        visitedMaze[i][j] = '.';
+    };
 
-  visit(maze, entrance, 0);
-  return answer.length <= 0 ? -1 : Math.min(...answer);
+    visit(maze, entrance, 0);
+    return answer.length <= 0 ? -1 : Math.min(...answer);
 };
+console.log(
+    nearestExit(
+        [
+            ['+', '+', '.', '+'],
+            ['.', '.', '.', '+'],
+            ['+', '+', '+', '.'],
+        ],
+        [1, 2],
+    ),
+);
---
+++ hojun/Implements/numWaterBottles.js
@@ -0,0 +1,16 @@
+const exchange = (bottles, unit) => [parseInt(bottles / unit), bottles % unit];
+
+const numWaterBottles = (numBottles, numExchange) => {
+    let empty = numBottles;
+    let drunk = numBottles;
+
+    do {
+        const [water, emptyBottle] = exchange(empty, numExchange);
+        drunk += water;
+        empty = emptyBottle + water;
+    } while (empty >= numExchange);
+
+    return drunk;
+};
+
+console.log(numWaterBottles(15, 4));
