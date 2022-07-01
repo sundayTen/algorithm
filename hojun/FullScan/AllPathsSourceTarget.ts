c18b577a274ae2bcc5426cb6cdbd80ba82a4deba
hojunin <dlsghwns@naver.com>
2022-07-02 00:08:42 +0900

    ?

--- hojun/FullScan/AllPathsSourceTarget.ts
+++ hojun/FullScan/AllPathsSourceTarget.ts
@@ -2,16 +2,18 @@ type NodeVal = number;
 type Path = NodeVal[];
 type Graph = Path[];
 
-const isDone = (endNode: NodeVal, node: NodeVal) => endNode === node;
+const isDoneChecker = (endNode: NodeVal) => (node: NodeVal) => endNode === node;
 
 const allPathsSourceTarget = (graph: Graph) => {
     const end = graph.length - 1;
     const foundPath: Path[] = [];
 
+    const isDone = isDoneChecker(end);
+
     const findPath = (path: Path, targetIndex: NodeVal) => {
         const newPath = [...path, targetIndex];
 
-        if (isDone(end, targetIndex)) {
+        if (isDone(targetIndex)) {
             foundPath.push(newPath);
         }
 
