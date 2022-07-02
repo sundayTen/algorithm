e2d9b1438a0d0f7f1e005bd6d10ccde93180f87c
hojunin <dlsghwns@naver.com>
2022-07-03 00:04:01 +0900

    doneee

--- hojun/Implements/deckRevealedIncreasing.js
+++ hojun/Implements/deckRevealedIncreasing.js
@@ -1,5 +1,14 @@
-/**
- * @param {number[]} deck
- * @return {number[]}
- */
-const deckRevealedIncreasing = (deck) => {};
+const deckRevealedIncreasing = (deck) => {
+    const sortedDeck = [...deck].sort((a, b) => a - b);
+    const remainIndexes = deck.map((_, index) => index);
+    const popOrder = [];
+
+    while (popOrder.length < deck.length) {
+        popOrder.push(remainIndexes.shift());
+        remainIndexes.push(remainIndexes.shift());
+    }
+
+    const answer = Array(deck.length).fill(-1);
+    popOrder.forEach((ans, index) => (answer[ans] = sortedDeck[index]));
+    return real;
+};
