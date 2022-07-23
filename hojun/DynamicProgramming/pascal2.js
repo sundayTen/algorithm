565ca178523b8204b432cf36b7ef7e0b0e6341b7
Nick <dlsghwns@naver.com>
2022-07-23 22:55:15 +0900

    구에ㅔ에엑

---
+++ hojun/DynamicProgramming/pascal2.js
@@ -0,0 +1,23 @@
+const createNextRow = (row) => {
+  return row
+    .map((_, ind, arr) => {
+      if (ind + 1 > arr.length - 1) {
+        return;
+      }
+
+      return arr[ind] + arr[ind + 1];
+    })
+    .filter((a) => a);
+};
+
+const getRow = (numRows) => {
+  const firstRow = [1];
+  const pascal = [firstRow];
+
+  for (let i = 1; i < numRows + 1; i++) {
+    const nextRow = [1, ...createNextRow(pascal[i - 1]), 1];
+    pascal.push(nextRow);
+  }
+
+  return pascal[numRows];
+};
